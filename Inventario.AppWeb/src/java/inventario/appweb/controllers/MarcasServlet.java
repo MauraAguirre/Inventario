fda0f13097e53e7e7d8a8bc1a73fa99b6f9040f8
maura aguirre <mauagui2000@gmail.com>
2023-08-21 16:46:15 -0600

    Codificacion del controlador de marcas

--- Inventario.AppWeb/src/java/inventario/appweb/controllers/MarcasServlet.java
+++ Inventario.AppWeb/src/java/inventario/appweb/controllers/MarcasServlet.java
@@ -9,9 +9,197 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.util.ArrayList;
+import inventario.accesoadatos.MarcasDAL;
+import inventario.entidadesdelnegocio.Marcas;
+import inventario.appweb.utils.*;
 
 @WebServlet(name = "MarcasServlet", urlPatterns = {"/Marcas"})
 public class MarcasServlet extends HttpServlet {
 
+     private Marcas obtenerMarcas(HttpServletRequest request) {
+        String accion = Utilidad.getParameter(request, "accion", "index");
+        Marcas marca = new Marcas();
+        if (accion.equals("create") == false) {
+            marca.setId(Integer.parseInt(Utilidad.getParameter(request, "id", "0")));
+        }
+
+        marca.setNombre(Utilidad.getParameter(request, "nombre", ""));
+        
+        if (accion.equals("index")) {
+            marca.setTop_aux(Integer.parseInt(Utilidad.getParameter(request, "top_aux", "10")));
+            marca.setTop_aux(marca.getTop_aux() == 0 ? Integer.MAX_VALUE : marca.getTop_aux());
+        }
+        
+        return marca;
+    }
     
+    private void doGetRequestIndex(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Marcas marca = new Marcas();
+            marca.setTop_aux(10);
+            ArrayList<Marcas> marcas = MarcasDAL.buscar(marca);
+            request.setAttribute("marcas", marcas);
+            request.setAttribute("top_aux", marca.getTop_aux());             
+            request.getRequestDispatcher("Views/Marcas/index.jsp").forward(request, response);
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+    
+    private void doPostRequestIndex(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Marcas marca = obtenerMarcas(request);
+            ArrayList<Marcas> marcas = MarcasDAL.buscar(marca);
+            request.setAttribute("contactos", marcas);
+            request.setAttribute("top_aux", marca.getTop_aux());
+            request.getRequestDispatcher("Views/Marcas/index.jsp").forward(request, response);
+        } catch (Exception ex) { 
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+    
+    private void doGetRequestCreate(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("Views/Marcas/create.jsp").forward(request, response);
+    }
+    
+    private void doPostRequestCreate(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Marcas marca = obtenerMarcas(request);
+            int result = MarcasDAL.crear(marca);
+            if (result != 0) {
+                request.setAttribute("accion", "index");
+                doGetRequestIndex(request, response);
+            } else {
+                Utilidad.enviarError("No se logró agregar un nuevo registro de marca", request, response);
+            }
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+    
+    private void requestObtenerPorId(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Marcas marca = obtenerMarcas(request);
+            Marcas marca_result = MarcasDAL.obtenerPorId(marca);
+            if (marca_result.getId() > 0) {
+                request.setAttribute("marca", marca_result);
+            } else {
+                Utilidad.enviarError("El Id:" + marca.getId() + " no existe en la tabla de Marcas", request, response);
+            }
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+    
+    private void doGetRequestEdit(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        requestObtenerPorId(request, response);
+        request.getRequestDispatcher("Views/Marcas/edit.jsp").forward(request, response);
+    }
+    
+    private void doPostRequestEdit(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Marcas marca = obtenerMarcas(request);
+            int result = MarcasDAL.modificar(marca);
+            if (result != 0) {
+                request.setAttribute("accion", "index");
+                doGetRequestIndex(request, response);
+            } else {
+                Utilidad.enviarError("No se logro actualizar el registro de la marca", request, response);
+            }
+        } catch (Exception ex) {
+            // Enviar al jsp de error si hay un Exception
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+    
+    private void doGetRequestDetails(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        requestObtenerPorId(request, response);
+        request.getRequestDispatcher("Views/Marca/details.jsp").forward(request, response);
+    }
+    
+    private void doGetRequestDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        requestObtenerPorId(request, response);
+        request.getRequestDispatcher("Views/Marca/delete.jsp").forward(request, response);
+    }
+    
+    private void doPostRequestDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Marcas marca = obtenerMarcas(request);
+            int result = MarcasDAL.eliminar(marca);
+            if (result != 0) {
+                request.setAttribute("accion", "index");
+                doGetRequestIndex(request, response);
+            } else {
+                Utilidad.enviarError("No se logró eliminar el registro de la marca", request, response);
+            }
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+    
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        SessionUser.authorize(request, response, () -> {
+            String accion = Utilidad.getParameter(request, "accion", "index");
+            switch (accion) {
+                case "index":
+                    request.setAttribute("accion", accion);
+                    doGetRequestIndex(request, response);
+                    break;
+                case "create":
+                    request.setAttribute("accion", accion);
+                    doGetRequestCreate(request, response);
+                    break;
+                case "edit":
+                    request.setAttribute("accion", accion);
+                    doGetRequestEdit(request, response);
+                    break;
+                case "delete":
+                    request.setAttribute("accion", accion);
+                    doGetRequestDelete(request, response);
+                    break;
+                case "details":
+                    request.setAttribute("accion", accion);
+                    doGetRequestDetails(request, response);
+                    break;
+                default:
+                    request.setAttribute("accion", accion);
+                    doGetRequestIndex(request, response);
+            }
+        });
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        SessionUser.authorize(request, response, () -> {
+            String accion = Utilidad.getParameter(request, "accion", "index");
+            switch (accion) {
+                case "index":
+                    request.setAttribute("accion", accion);
+                    doPostRequestIndex(request, response);
+                    break;
+                case "create":
+                    request.setAttribute("accion", accion);
+                    doPostRequestCreate(request, response);
+                    break;
+                case "edit":
+                    request.setAttribute("accion", accion);
+                    doPostRequestEdit(request, response);
+                    break;
+                case "delete":
+                    request.setAttribute("accion", accion);
+                    doPostRequestDelete(request, response);
+                    break;
+                default:
+                    request.setAttribute("accion", accion);
+                    doGetRequestIndex(request, response);
+            }
+        });
+    }
+// </editor-fold>
 }
--- Inventario.AppWeb/src/java/inventario/appweb/utils/SessionUser.java
+++ Inventario.AppWeb/src/java/inventario/appweb/utils/SessionUser.java
@@ -1,13 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package inventario.appweb.utils;
 
-/**
- *
- * @author MINEDUCYT
- */
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import inventario.entidadesdelnegocio.*;
+
 public class SessionUser {
+    public static void autenticarEmpleado(HttpServletRequest request, Empleado pEmpleado) {
+        HttpSession session = (HttpSession) request.getSession();
+        session.setMaxInactiveInterval(3600);
+        session.setAttribute("auth", true);
+        session.setAttribute("emple", pEmpleado.getUsuario());
+        session.setAttribute("rol", pEmpleado.getRol().getNombre()); 
+    }
     
+    public static boolean isAuth(HttpServletRequest request) {
+        HttpSession session = (HttpSession) request.getSession();
+        boolean auth = session.getAttribute("auth") != null ? (boolean) session.getAttribute("auth") : false;
+        return auth;
+    }
+    
+    public static String getEmpleado(HttpServletRequest request) {
+        HttpSession session = (HttpSession) request.getSession();
+        String emple = "";
+        if (SessionUser.isAuth(request)) { 
+            emple = session.getAttribute("empleado") != null ? (String) session.getAttribute("empleado") : "";
+        }
+        return emple;
+    }
+    
+    public static String getRol(HttpServletRequest request) {
+        HttpSession session = (HttpSession) request.getSession();
+        String user = "";
+        if (SessionUser.isAuth(request)) {
+            user = session.getAttribute("rol") != null ? (String) session.getAttribute("rol") : "";
+        }
+        return user;
+    }
+    
+    public static void authorize(HttpServletRequest request, HttpServletResponse response, IAuthorize pIAuthorize) throws ServletException, IOException {
+        if (SessionUser.isAuth(request)) {
+            pIAuthorize.authorize();
+        } else {
+            response.sendRedirect("Empleado?accion=Usuario");
+        }
+    }
+    
+    public static void cerrarSession(HttpServletRequest request) {
+        HttpSession session = (HttpSession) request.getSession();
+        session.invalidate();
+    }
 }
